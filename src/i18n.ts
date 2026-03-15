1043a5f57c1ddf718cf52a752da6a79da548f8f8
Адель Мажит <adel.mazhit@gmail.com>
2026-03-15 17:02:49 +0000

    кнопка sign in в registration window не открывается

--- src/i18n.ts
+++
@@ -1,8 +0,0 @@
-import {getRequestConfig} from 'next-intl/server';
-
-export default getRequestConfig(async ({locale}) => {
-  // This will be validated by the middleware, so we don't need to check it here.
-  return {
-    messages: (await import(`./messages/${locale}.json`)).default
-  };
-});
