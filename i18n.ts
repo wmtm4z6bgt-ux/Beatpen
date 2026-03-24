034ceab61e73f770029b77df138b607fc2ee5758
Адель Мажит <adel.mazhit@gmail.com>
2026-03-24 05:20:23 +0000

    Try fixing this error: `Console Error: A `locale` is expected to be retu

--- i18n.ts
+++ i18n.ts
@@ -3,6 +3,7 @@ import {getRequestConfig} from 'next-intl/server';
 export default getRequestConfig(async ({locale}) => {
   // This will be validated by the middleware, so we don't need to check it here.
   return {
+    locale,
     messages: (await import(`./src/messages/${locale}.json`)).default
   };
 });
